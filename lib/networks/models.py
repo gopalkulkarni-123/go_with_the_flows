fb07c414f94b377675b6d34728909f26c44a6c43
unknown <gopaldamodarkulkarni@gmail.com>
2022-11-04 12:52:40 +0100

    input size

--- lib/networks/models.py
+++ lib/networks/models.py
@@ -243,8 +243,8 @@ class Local_Cond_RNVP_MC_Global_RNVP_VAE(nn.Module):
             output_decoder: samples list after decoder flow
             mixture_weights_logits: log weight of each flow.
         '''
-        print("p_input is ", p_input)
-        print("g_input is ", g_input)
+        print("p_input is ", p_input.size())
+        print("g_input is ", g_input.size())
 
         sampled_cloud_size = p_input.shape[2] if n_sampled_points is None else n_sampled_points
         if images is not None and self.train_mode == 'p_rnvp_mc_g_rnvp_vae_ic':
