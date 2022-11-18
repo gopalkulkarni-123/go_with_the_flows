64d67677ee079feccc703138f4db8a15516e780b
unknown <gopaldamodarkulkarni@gmail.com>
2022-11-18 13:06:28 +0100

    weights encoder

--- lib/networks/encoders.py
+++ lib/networks/encoders.py
@@ -86,5 +86,5 @@ class WeightsEncoder(FeatureEncoder):
     def forward(self, input):
         mus = super().forward(input)
         weights = nn.functional.log_softmax(mus, dim=1)
-        print("Weights from WeightEncoder is" weights)
+        print("Weights from WeightEncoder is", weights)
         return weights
